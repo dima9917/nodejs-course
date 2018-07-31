5f7335c01116b311bb403179f5ead78c02193656
dima9917 <dima9917@gmail.com>
2018-07-31 10:53:47 -0700

    ....

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,8 +2,13 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
-
+const notes = require('./notes.js');
 var user = os.userInfo();
-console.log(user);
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+var a = 1, b = 2;
+
+console.log(notes.add(a, b));
+
+// console.log(user);
+
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
