6619ce7f4e12fa4cdd2cfa1f3be44fd1192ffb71
dima9917 <dima9917@gmail.com>
2018-08-05 15:57:56 -0700

    S3L19

--- notes-node/playgound/json.js
+++ notes-node/playgound/json.js
@@ -1,33 +1,25 @@
 // var obj = {
-//     name: 'Dima'
+//   name: 'Andrew'
 // };
-
 // var stringObj = JSON.stringify(obj);
-
-// console.log(typeof(stringObj));
+// console.log(typeof stringObj);
 // console.log(stringObj);
-// console.log(typeof(obj));
-// console.log(obj);
 
-// var personString = '{"name": "Dima" ,"age": 29}';
+// var personString = '{"name": "Andrew","age": 25}';
 // var person = JSON.parse(personString);
-// console.log(typeof(person));
+// console.log(typeof person);
 // console.log(person);
 
 const fs = require('fs');
 
 var originalNote = {
-    title: 'Some title',
-    body: 'Some body'
-}
-
+  title: 'Some title',
+  body: 'Some body'
+};
 var originalNoteString = JSON.stringify(originalNote);
-
-fs.writeFileSync('notes.json', originalNoteString)
+fs.writeFileSync('notes.json', originalNoteString);
 
 var noteString = fs.readFileSync('notes.json');
-
-var note = JSON.parse(noteString)
-
-console.log(typeof(note));
+var note = JSON.parse(noteString);
+console.log(typeof note);
 console.log(note.title);
